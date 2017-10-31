bb69035a5c4fd243d4fc59d63bc6413d4c501046
Christian Genco <christian.genco@gmail.com>
2017-10-30 20:34:47 -0500

    add gen.co redirect script and config

---
+++ x/sync_redirects.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+require 'json'
+
+`echo > /tmp/blank`
+
+JSON.parse(File.read('redirects.json')).each do |path, url|
+  puts "#{path} => #{url}"
+  `aws s3 cp /tmp/blank s3://gen.co/#{path} --website-redirect="#{url}?utm_source=genco&utm_campaign=redirect"`
+end

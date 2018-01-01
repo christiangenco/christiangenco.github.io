b7f453cc83d078bbb99d7c917e2d99b9f064d63d
Christian Genco <christian.genco@gmail.com>
2018-01-01 15:30:22 -0600

    gen.co/2018

--- x/sync_redirects.rb
+++ x/sync_redirects.rb
@@ -5,6 +5,6 @@ require 'json'
 `echo > /tmp/blank`
 
 JSON.parse(File.read('redirects.json')).each do |path, url|
-  puts "#{path} => #{url}"
+  puts "https://gen.co/#{path} => #{url}"
   `aws s3 cp /tmp/blank s3://gen.co/#{path} --website-redirect="#{url}?utm_source=genco&utm_campaign=redirect"`
 end

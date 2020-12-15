dc5416803fd9173665a9e8fa16786d938b33b187
Christian Genco <christian.genco@gmail.com>
2020-12-14 18:06:05 -0600

    update buildAssets to ignore .DS_Store

--- x/buildAssets.sh
+++ x/buildAssets.sh
@@ -6,13 +6,13 @@ mkdir -p build/css
 # mkdir -p build/images
 # rsync -av assets/images/ build/images/
 
-rsync -av assets/ build/
+rsync -av -exclude='.DS_Store' assets/ build/
 
 # set NODE_ENV=production to get minified styles
 
-# NODE_ENV=production node_modules/postcss-cli/bin/postcss assets/css/style.css -o build/css/style.css
+NODE_ENV=production node_modules/postcss-cli/bin/postcss assets/css/style.css -o build/css/style.css
 
-node_modules/postcss-cli/bin/postcss assets/css/style.css -o build/css/style.css
+# node_modules/postcss-cli/bin/postcss assets/css/style.css -o build/css/style.css
 
 echo "style-`md5 -q build/css/style.css | head -c 4`.css" > build/css/latestStyleBasename.txt
 cp build/css/style.css build/css/`cat build/css/latestStyleBasename.txt`

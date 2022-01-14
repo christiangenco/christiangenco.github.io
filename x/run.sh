96d9711342d72409f2b9453c02ec198d9a74a263
Christian Genco <christian@gen.co>
2022-01-13 20:48:06 -0600

    add /air redirect

--- x/run.sh
+++ x/run.sh
@@ -1,1 +1,2 @@
+echo run this with x/watch
 pushd build && python -m SimpleHTTPServer; popd
--- x/sync_redirects.rb
+++ x/sync_redirects.rb
@@ -2,11 +2,12 @@
 
 require 'json'
 
-# `echo > /tmp/blank`
+`echo > /tmp/blank`
 
 JSON.parse(File.read('redirects.gen.co.json')).each do |path, url|
   # unless path.include "christian.gen.co"
   puts "https://gen.co/#{path} => #{url}"
-  `aws s3 cp /tmp/blank s3://gen.co/#{path} --website-redirect="#{url}?utm_source=genco&utm_campaign=redirect"`
+  `aws s3 cp /tmp/blank s3://gen.co/#{path} --website-redirect="#{url}"`
+  # ?utm_source=genco&utm_campaign=redirect
   # end
 end
